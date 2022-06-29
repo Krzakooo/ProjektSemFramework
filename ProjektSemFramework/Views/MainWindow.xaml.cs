af6eba5a2ece6d61728db7bd42431fd0844c0939
Krzakooo <55135961+Krzakooo@users.noreply.github.com>
2022-06-30 01:29:37 +0200

    Add project files.

---
+++ ProjektSemFramework/Views/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MahApps.Metro.Controls;
+using ProjektSemFramework.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektSemFramework
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += WindowLoaded;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            frame.NavigationService.Navigate(new Display());
+        }
+    }
+}
