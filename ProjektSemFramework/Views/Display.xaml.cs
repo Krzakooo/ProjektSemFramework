375155b5aa2c781ec4deb17b46de255b39795f9e
Krzakooo <55135961+Krzakooo@users.noreply.github.com>
2022-07-01 00:20:06 +0200

    Created database, added a first draft of GUI, started configuring page "Display" which shows all translators and their attributes

--- ProjektSemFramework/Views/Display.xaml.cs
+++ ProjektSemFramework/Views/Display.xaml.cs
@@ -23,12 +23,50 @@ namespace ProjektSemFramework.Views
         public Display()
         {
             InitializeComponent();
+
+            TranslatorsDBEntities db = new TranslatorsDBEntities();
+            var Ts = from d in db.Tlumaczes
+                     join b in db.Jezyki_Tlumacza on d.id_tlumacza equals b.id_tlumacza
+                     join a in db.Jezykis on b.id_jezyka equals a.id_jezyka
+                     select new
+                     {
+                         Translator = d.imie + " " + d.nazwisko,
+                         NativeSpeaker = d.jezyk_ojczysty,
+                         Language = a.jezyk
+                     };
+
+            var Ls = from d in db.Tlumaczes
+                     select new
+                     {
+                         Languages = d.Jezyki_Tlumacza
+                     };
+
+            foreach (var item in Ts)
+            {
+                //Console.WriteLine(item.nazwisko);
+            }
+
+            this.TranslatorGrid.ItemsSource = Ts.ToList(); 
         }
 
 
         private void BtnClick1(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new Order());
+        }
+
+        private void BtnClickAdd(object sender, RoutedEventArgs e)
+        {
+            TranslatorsDBEntities db = new TranslatorsDBEntities();
+            Tlumacze tlumaczeObject = new Tlumacze()
+            {
+                imie = txtName.Text,
+                nazwisko = txtSurame.Text,
+                jezyk_ojczysty = txtLang.Text,
+            };
+
+            db.Tlumaczes.Add(tlumaczeObject);
+            db.SaveChanges();
         }
     }
 }
